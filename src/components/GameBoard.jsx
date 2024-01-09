df73a2a1d58959f1ad02d6ed828b40bdc22a5c41
Tiago Gomes <tidgomes@gmail.com>
2024-01-09 13:50:41 -0300

    make GameBoard component stateful

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,19 +1,31 @@
-const initialBoardGame = [
+import { useState } from "react"
+
+const initialStateBoardGame = [
   [null, null, null],
   [null, null, null],
   [null, null, null]
 ]
 
 export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialStateBoardGame)
+
+  function handleSelectSquare(rowIndex, colIndex, symbol)  {
+    setGameBoard(prevState => {
+      const updatedBoard = [...prevState.map(innerArray => [...innerArray])]
+      updatedBoard[rowIndex][colIndex] = '$'
+      return updatedBoard
+    })
+  }
+
   return (
     <ol id='game-board'>
-      {initialBoardGame.map((row, rowIndex) => {
+      {gameBoard.map((row, rowIndex) => {
         return (
           <li key={rowIndex}>
             <ol>
               {row.map((playerSymbol, colIndex) => (
                 <li key={colIndex}>
-                  <button>{playerSymbol}</button>
+                  <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
                 </li>
               ))}
             </ol>
