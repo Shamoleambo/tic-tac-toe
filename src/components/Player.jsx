384a8c918c2d7b86c70c0ef4006e66eb9106cb3a
Tiago Gomes <tidgomes@gmail.com>
2024-01-09 12:53:59 -0300

    make Player component a stateful component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,29 @@
+import { useState } from 'react'
+
 export default function Player({ name, symbol }) {
+  const [playerName, setPlayerName] = useState(name)
+  const [editMode, setEditMode] = useState(false)
+
+  const handlePlayerName = (event) => {
+    setPlayerName(event.target.value)
+  }
+
+  const handleEditClick = () => {
+    setEditMode((prevState) => !prevState)
+  }
   return (
     <li>
       <span className='player'>
-        <span className='player-name'>{name}</span>
+        {editMode === true ? (
+          <input type='text' onChange={handlePlayerName} value={playerName}></input>
+        ) : (
+          <span className='player-name'>{playerName}</span>
+        )}
         <span className='player-symbol'>{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>
+        {editMode === true ? 'Save' : 'Edit'}
+      </button>
     </li>
   )
 }
